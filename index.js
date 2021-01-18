99b04b85906eee129469d0e7a15acea9d294d809
Rūta Sakalauskaitė <ruta.sakalauskaite@vmg.eu>
2021-01-18 12:25:06 +0200

    starting app wit express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use('/js', express.static(__dirname + '/views/js/'));
+app.use('/styles', express.static(__dirname + '/views/styles'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', function(req, res) {
+    res.render("index.html");
+})
+
+const webserver = app.listen(8080, function() {
+    console.log('Node web server is running...');
+})
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Starting Outlook calendar app"
-npx http-server -c-1
